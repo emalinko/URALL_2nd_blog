ecc6ad9fb22a3b2374c7090b6509ad52c8accf0e
malinko <malin88able@gmail.com>
2021-01-23 16:41:56 +0200

    Init

---
+++ URALL_2nd-root/URALL_2nd/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for URALL_2nd project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'URALL_2nd.settings')
+
+application = get_wsgi_application()
